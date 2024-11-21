e11a0bb54c566a4ccea10a1733af08cd5b139b0a
Anel Ariadna Toledo Balcazar <anel.toledo@softtekprojects.com>
2024-11-21 17:15:14 -0600

    agregue ejercicios de la clase 1

---
+++ Ejercicios1/src/ejercicio/constantes/Constantes.java
@@ -0,0 +1,12 @@
+package ejercicio.constantes;
+
+public class Constantes {
+	public static void main(String[] args)
+	{
+		final double PI = 3.14159;
+		double radio = 10, area;
+		area = PI * (Math.pow(radio, 2));
+		System.out.println("El area del circulo es: " + area);
+		
+	}
+}
---
+++ Ejercicios1/src/ejercicio/variables/Variables.java
@@ -0,0 +1,15 @@
+package ejercicio.variables;
+
+public class Variables {
+	
+	public static void main(String[] args)
+	{	int años = 20;
+		años += 5;
+		System.out.println(años);
+		Integer hijos = 2;
+		String nombre ="Anel", apellido="Toledo";
+		System.out.println("Hola mi nombre es: "+ nombre + " "+ apellido+ ", tengo " + años + " y tengo " + hijos + " hijos");
+	
+		
+	}
+}
