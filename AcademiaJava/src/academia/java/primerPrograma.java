428ae979f40725b994e090842a96084ee23b0006
Jocelyn Amacalli Loranca Cortes <jocelyn.loranca@softtekprojects.com>
2024-11-21 17:15:13 -0600

    Agregue ejercicio variables

---
+++ AcademiaJava/src/academia/java/primerPrograma.java
@@ -0,0 +1,21 @@
+//Primer programa
+//mi nombre es y mis hijos mas decrementacion 5 anios
+
+package academia.java;
+
+public class primerPrograma {
+	public static void main(String[] args) {
+
+		// datos primitivos
+		//int anio = 20 + 5;
+		int hijos = 2;
+		int anio = 20;
+
+		String nombre = "Jocelyn";
+		String apellido = "Loranca";
+		anio +=5;
+
+		System.out.println("Hola mi nombre es: " + nombre + " " + apellido);
+		System.out.println("Tengo " + anio + " años y tengo " + hijos + " hijos");
+	}
+}
---
+++ AcademiaJava/src/academia/java/segundoPrograma.java
@@ -0,0 +1,16 @@
+//Segundo programa que calcule el area de un circulo 
+
+package academia.java;
+
+public class segundoPrograma {
+	public static void main(String[] args) {
+		
+		double pi = 3.1416;
+		double radio = 10;
+		double area = pi * radio * radio;
+		
+		System.out.println("Hola, el area de un circulo con radio "
+				+ radio + " es: " + area);
+	}
+
+}
