ca7c39a8a31a95e354167c48e41e49abf08f360a
Aislinn Alison Barrera Rivera <aislinn.barrera@softtekprojects.com>
2024-11-21 17:16:38 -0600

    agregue ejercicios de la clase 1

---
+++ academia/Programa1.java
@@ -0,0 +1,20 @@
+package com.academia;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int anios =20;
+anios +=5;
+System.out.println("nuevo valor de años: "+anios);
+
+Integer hijos = 2;
+
+String nombre = "Aislinn Alison";
+String apellido = "Barrera Rivera";
+
+System.out.println("Hola, mi nombre es: "+ nombre+" "+apellido+", tengo "+anios+ " años y tengo"+hijos+ " hijos.");
+
+	}
+
+}
---
+++ academia/Programa2.java
@@ -0,0 +1,13 @@
+package com.academia;
+
+public class Programa2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+final double PI= 3.14159;
+double radio =10;
+double area = PI * radio * radio;
+System.out.println("El area del circulo con radio "+radio+" es: "+area);
+	}
+
+}
