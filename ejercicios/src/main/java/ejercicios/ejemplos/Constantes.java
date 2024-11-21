24f8a0fd9c98c3d34346ac7d4c4d21a549bd4a01
Efrain Cruz Lobato <efrain.cruz@softtekprojects.com>
2024-11-21 17:16:27 -0600

    Subiendo ejercicios de practica dia1

---
+++ ejercicios/src/main/java/ejercicios/ejemplos/Constantes.java
@@ -0,0 +1,15 @@
+package ejercicios.ejemplos;
+
+public class Constantes {
+
+	public static void main(String[] args) {
+		// CREA CONSTANTE
+		final double PI = 3.14159;
+		// CREAR VARIABLE
+		int RADIO = 10;
+		// REALIZAR OPERACION
+		double area = PI * RADIO * RADIO;
+		// IMPRIMIMOS MENSAJE Y RESULTADO
+		System.out.println("El area es: " + area);
+	}
+}
---
+++ ejercicios/src/main/java/ejercicios/ejemplos/Variables.java
@@ -0,0 +1,22 @@
+package ejercicios.ejemplos;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// CREAR VARIABLES
+		int años = 20;
+		// MODIFICAR VARIABLES
+		años += 5;
+		// IMPRIMIR NUEVO VALOR
+		System.out.println("El nuevo valor de la variable es: " + años);
+		// VARIABLES WRAPPER
+		Integer hijos = 2;
+		String nombre = "Efrain";
+		String apellidos = "Cruz Lobato";
+		// IMPRIMIMOS MENSAJE Y RESULTADO
+		System.out.println("Hola mi nombre es " + nombre + " " + apellidos + ", tengo " + años + " años " + "y tengo "
+				+ hijos + " hijos.");
+
+	}
+
+}
