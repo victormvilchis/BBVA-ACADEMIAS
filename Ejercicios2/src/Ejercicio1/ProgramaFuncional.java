7787f2c0d9d69392db9e311eea8e1a56937b1a7d
Anel Ariadna Toledo Balcazar <anel.toledo@softtekprojects.com>
2024-11-22 13:40:02 -0600

    agregue ejercicios de la clase 2

--- Ejercicios2/src/Ejercicio1/ProgramaFuncional.java
+++ Ejercicios2/src/Ejercicio1/ProgramaFuncional.java
@@ -3,12 +3,12 @@ package Ejercicio1;
 import java.util.Scanner;
 
 public class ProgramaFuncional {
-    private static final double promAprobada = 6.0; 
 
     public static void main(String[] args) {
+        final double promAprobada = 6.0; // calificacion minima 
         Scanner scanner = new Scanner(System.in);
-        double promedio;
-        int aciertos, reactivos; 
+        Double promedio;
+        Integer aciertos, reactivos; 
         String calAprobatoria = "reprobado";
         
         System.out.println("Ingrese el numero de reactivos: " );
@@ -18,19 +18,20 @@ public class ProgramaFuncional {
         promedio = ((double) aciertos / reactivos);  
         promedio *= 10;
         
-        if(promedio>= promAprobada)
+  /*      if(promedio>= promAprobada)
         {
         	calAprobatoria = "aprobado";
         }
         else
         {
         	calAprobatoria = "reprobado";
-        }
+        } */
         
         calAprobatoria = promedio >= promAprobada ? "aprobado" : "reprobado";
         System.out.println("El numero de reactivos es: " +reactivos);
-        System.out.println("El alumno está " + calAprobatoria);
         System.out.println("La calificación de tu examen fue de: "+ promedio);
+        System.out.println("El alumno está " + calAprobatoria);
+        
         if(calAprobatoria=="aprobado")
         {
         	System.out.println("Felicidades");
