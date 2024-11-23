1d9502fe8f6ffbfbc371a68cfab209561ce216e3
Kevin Nathanael Martinez Barrera <kevinn.martinez@softtekprojects.com>
2024-11-22 18:05:58 -0600

    Actualizado

--- Viernes_22/EJ2.java
+++ Viernes_22/EJ2.java
@@ -3,35 +3,34 @@ package Viernes_22;
 public class EJ2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		final double pi = 3.1416;
-		double radio = 10;
-		double height = 10;
+		final Double pi = 3.1416;
+		Double radio = 10.12;
+		Double height = 15.14;
 		
 		lateralArea(pi, radio, height);
 		totalArea(pi, radio);
 		volume(pi, radio, height);
 	}
 	
-	public static void lateralArea(double pi, double radio, double height) {
+	public static void lateralArea(Double pi, Double radio, Double height) {
 
-		Double lateralArea = new Double((2 * pi * radio * height));
+		Double lateralArea = (2 * pi * radio * height);
 		
 		System.out.println("El area lateral es: " + lateralArea);
 	}
 	
-	public static void totalArea(double pi, double radio) {
+	public static void totalArea(Double pi, Double radio) {
 		
-		Double totalArea = new Double((2 * pi * (radio * radio)));
+		Double totalArea = ((2 * pi * (radio * radio))); 
 			
 		System.out.println("El area total del cilindro es: " + totalArea);
-		}
+	}
 	
-	public static void volume(double pi, double radio, double height) {
+	public static void volume(Double pi, Double radio, Double height) {
 		
-		Double volume = new Double((2 * pi * (radio * radio) * height));
+		Double volume = (2 * pi * (radio * radio) * height);
 		
 		System.out.println("El volumen del cilindro es: " + volume);
 	}
-
+	
 }
